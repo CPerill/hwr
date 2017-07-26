aae9e6124fa3f991b7efceadb99c597ec0c1e035
connor-jos-perill <connor.jos.perill@hpe.com>
2017-07-26 14:40:44 +0100

    Added App

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-	<header>
-		<nav>
-			<ul>
-				<li><Link to='/'>Home</Link></li>
-				<li><Link to='/detail/:repo'>React</Link></li>
-			</ul>
-		</nav>
-	</header>
-)
-
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Unofficial GitHub Browser v0.1</h1>
-                <Header />
-                
+                <h1><Link to='/'>Unofficial GitHub Browser v0.1</Link></h1>
             </div>
         );
     }
